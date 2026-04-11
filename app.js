85a3be67629d1de9bf7542a1bba09802e19f642c
Mehnaz <mailmehnaz@gmail.com>
2026-04-11 16:30:14 +0530

    added debussing to calculate diatance

--- app.js
+++ app.js
@@ -839,6 +839,7 @@ const a = Math.sin(dLat/2) * Math.sin(dLat/2) +
 Math.cos(hop1.lat * Math.PI / 180) * Math.cos(hop2.lat * Math.PI / 180) *
 Math.sin(dLng/2) * Math.sin(dLng/2);
 const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+console.log("inaside calculate distance", R*c);
 return R * c;
 }
 
